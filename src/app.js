bb1efbe414d344be5d186c461f3da99edd43807b
Pimple <araozlucas@gmail.com>
2025-09-21 16:55:23 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ['My dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function randomElement (arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+window.onload = () => {
+  document.getElementById("excuse").innerHTML =
+    `${randomElement(who)} ${randomElement(action)} ${randomElement(what)} ${randomElement(when)}.`;
+    //write your code here
 };
